e22e28aab985c536c4d0d943870247bba653fa94
Martin Kennish <billthemaxster@gmail.com>
2020-08-28 23:26:35 +0100

    Create a grid struct for encapsulating that functionality

--- src/lib.rs
+++ src/lib.rs
@@ -11,22 +11,40 @@ pub struct Tile {
 }
 
 #[derive(Debug)]
-pub struct Game {
-    pub score: u32,
-    pub tiles: Vec<Vec<Tile>>,
+pub struct Grid {
+    data: Vec<Vec<Option<Tile>>>,
+    size: u8,
 }
 
-impl Game {
-    pub fn new(size: u8) -> Result<Game, &'static str> {
+impl Grid {
+    pub fn new(size: u8) -> Result<Grid, &'static str> {
         if size < 2 {
             return Err("cannot create a game with a grid smaller than 2x2.");
         }
 
-        let size: usize = size.into();
-        let score = 0;
-        let tiles: Vec<Vec<Tile>> = vec![vec![Tile { value: 2 }; size]; size];
+        let array_size: usize = size.into();
+        let data: Vec<Vec<Option<Tile>>> = vec![vec![None; array_size]; array_size];
 
-        Ok(Game { score, tiles })
+        Ok(Grid { size, data })
+    }
+
+    pub fn add_new_tile(value: u16) -> Result<(), &'static str> {
+        Err("not implemented")
+    }
+}
+
+#[derive(Debug)]
+pub struct Game {
+    pub score: u32,
+    pub grid: Grid,
+}
+
+impl Game {
+    pub fn new(size: u8) -> Result<Game, &'static str> {
+        let score = 0;
+        let grid: Grid = Grid::new(size)?;
+
+        Ok(Game { score, grid })
     }
 }
 
@@ -52,12 +70,7 @@ mod tests {
             fn set_tiles_to_grid_of_given_size(input: u8) {
                 let result = Game::new(input).unwrap();
 
-                let expected_length: usize = input.into();
-                assert_eq!(result.tiles.len(), expected_length);
-
-                for row in result.tiles.iter() {
-                    assert_eq!(row.len(), expected_length);
-                }
+                assert_eq!(result.grid.size, input);
             }
 
             #[rstest(input, case(0), case(1))]
@@ -68,6 +81,44 @@ mod tests {
                     result.unwrap_err(),
                     "cannot create a game with a grid smaller than 2x2."
                 );
+            }
+        }
+    }
+
+    mod grid {
+        use super::*;
+
+        mod new {
+            use super::*;
+            use rstest::rstest;
+
+            #[rstest(expected_size, case(2), case(4), case(5))]
+            fn set_size_to_given_size(expected_size: u8) {
+                let result = Grid::new(expected_size).unwrap();
+
+                assert_eq!(result.size, expected_size);
+            }
+
+            #[rstest(invalid_size, case(0), case(1))]
+            fn error_on_an_invalid_size(invalid_size: u8) {
+                let result = Game::new(invalid_size);
+
+                assert_eq!(
+                    result.unwrap_err(),
+                    "cannot create a game with a grid smaller than 2x2."
+                );
+            }
+
+            #[rstest(expected_size, case(2), case(4), case(5))]
+            fn set_dimensions_of_grid_data_to_given_size(expected_size: u8) {
+                let result = Grid::new(expected_size).unwrap();
+
+                let expected_length: usize = expected_size.into();
+                assert_eq!(result.data.len(), expected_length);
+
+                for row in result.data.iter() {
+                    assert_eq!(row.len(), expected_length);
+                }
             }
         }
     }
