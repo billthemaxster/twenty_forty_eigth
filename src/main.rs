c57bc4e477cd84b7fde8901e51a8608cedcc613b
Martin Kennish <billthemaxster@gmail.com>
2020-08-30 20:13:19 +0100

    use engine in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod engine;
+use crate::engine::game::*;
+
 fn main() {
     println!("Hello, world!");
+
+    let game: Game = Game::new(2).unwrap();
+
+    println!("Score: {}",game.get_score()); 
 }
