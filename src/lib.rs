2c18ea8a27495af53320c3924d310ce657f53e20
Martin Kennish <billthemaxster@gmail.com>
2020-08-30 14:04:56 +0100

    Add grid coord is equal method

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,12 @@ pub struct GridCoord {
     pub y: u8,
 }
 
+impl GridCoord {
+    pub fn is_equal(&self, other: &GridCoord) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct Tile {
     pub value: u16,
@@ -318,6 +324,55 @@ mod tests {
 
                 let empty_positions = grid.get_empty_positions();
                 assert_eq!(empty_positions.len(), expected_squares_count);
+            }
+
+        }
+    }
+
+    mod grid_coords {
+        use super::*;
+
+        mod is_equal{
+            use super::*;
+
+            #[test]
+            fn returns_true_when_both_x_and_y_are_equal(){
+                let coord1 = GridCoord{x:0, y:0};
+                let coord2 = GridCoord{x:0, y: 0};
+
+                let result = coord1.is_equal(&coord2);
+
+                assert_eq!(result, true);
+            }
+            
+            #[test]
+            fn returns_false_when_both_x_and_y_are_not_equal(){
+                let coord1 = GridCoord{x:0, y:0};
+                let coord2 = GridCoord{x:1, y: 1};
+
+                let result = coord1.is_equal(&coord2);
+
+                assert_eq!(result, false);
+            }
+            
+            #[test]
+            fn returns_false_when_xs_are_not_equal(){
+                let coord1 = GridCoord{x:0, y:0};
+                let coord2 = GridCoord{x:1, y: 0};
+
+                let result = coord1.is_equal(&coord2);
+
+                assert_eq!(result, false);
+            }
+            
+            #[test]
+            fn returns_false_when_ys_are_not_equal(){
+                let coord1 = GridCoord{x:0, y:0};
+                let coord2 = GridCoord{x:0, y: 1};
+
+                let result = coord1.is_equal(&coord2);
+
+                assert_eq!(result, false);
             }
         }
     }
